36d9df7b56952b668d90fb011cedc38079d49751
Артем <art-dikov@yandex.ru>
2020-03-29 21:03:28 +0400

    H5 Task1

--- src/ru/progwards/java2/lessons/tests/test/calc/CalculatorTest.java
+++ src/ru/progwards/java2/lessons/tests/test/calc/CalculatorTest.java
@@ -10,11 +10,40 @@ public class CalculatorTest {
     @Test
     public void sumTest(){
         SimpleCalculator calculator =  Mockito.mock(SimpleCalculator.class);
-        Mockito.when(calculator.sum(5,10)).thenReturn(100);
+        Mockito.when(calculator.sum(5,10)).thenReturn(15);
         int actual = calculator.sum(5,10);
-        int expected = 100;
+        int expected = 15;
 
         Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void multTest(){
+        SimpleCalculator calculator =  Mockito.mock(SimpleCalculator.class);
+        Mockito.when(calculator.mult(5,10)).thenReturn(50);
+        int actual = calculator.mult(5,10);
+        int expected = 50;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void divTest(){
+        SimpleCalculator calculator =  Mockito.mock(SimpleCalculator.class);
+        Mockito.when(calculator.div(5,10)).thenReturn(0);
+        int actual = calculator.div(5,10);
+        int expected = 0;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void diffTest(){
+        SimpleCalculator calculator =  Mockito.mock(SimpleCalculator.class);
+        Mockito.when(calculator.diff(5,10)).thenReturn(-5);
+        int actual = calculator.diff(5,10);
+        int expected = -5;
+
+        Assert.assertEquals(expected, actual);
+    }
 }
