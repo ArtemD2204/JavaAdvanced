3659768f260e81ebb1c6ec71a618bd8d965fa831
Артем <art-dikov@yandex.ru>
2020-03-31 01:36:08 +0400

    H5

---
+++ src/ru/progwards/java2/lessons/gc/Heap.java
@@ -0,0 +1,14 @@
+package ru.progwards.java2.lessons.gc;
+
+public class Heap {
+    private byte[] bytes;
+
+    Heap(int maxHeapSize) {
+        bytes = new byte[maxHeapSize];
+    }
+
+    public int malloc(int size) {
+        return 0;
+    }
+
+}
