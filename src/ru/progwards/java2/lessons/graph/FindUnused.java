8708e4f5652eaea9cc7b34097d63b89b4af4da39
Артем <art-dikov@yandex.ru>
2020-04-23 21:49:57 +0400

    H12: task2 fixed

--- src/ru/progwards/java2/lessons/graph/FindUnused.java
+++ src/ru/progwards/java2/lessons/graph/FindUnused.java
@@ -1,27 +1,20 @@
 package ru.progwards.java2.lessons.graph;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class FindUnused {
-    private static List<CObject> unused = new ArrayList<>();
 
     public static List<CObject> find(List<CObject> roots, List<CObject> objects){
-        unused.clear();
-        Queue<CObject> queue = new ArrayDeque<>();
-        queue.addAll(roots);
-        while (!queue.isEmpty()){
-            CObject cObject = queue.poll();
-            if (cObject.mark == 0) { // if unused
-                unused.add(cObject);
+        objects.forEach(o -> o.mark = 0); // make all unused
+        Deque<CObject> stack = new ArrayDeque<>(roots);
+        while (!stack.isEmpty()){
+            CObject cObject = stack.pollFirst();
+            if (cObject.mark != 1) { // if unvisited
                 cObject.mark = 1; // make visited
-            }else if (cObject.mark == 2){ // if used
-                queue.addAll(cObject.references);
-                cObject.mark = 1; // make visited
+                cObject.references.forEach(stack::offerFirst);
             }
         }
-        return unused;
+        objects.removeIf(cObject -> cObject.mark == 1);
+        return objects;
     }
 }
