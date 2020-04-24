af224c25a3bc85e916bad9f98df62b0dc8bc8c11
Артем <art-dikov@yandex.ru>
2020-04-24 15:45:56 +0400

    H13: task1 in progress

---
+++ src/ru/progwards/java2/lessons/synchro/app/service/FileStoreService.java
@@ -0,0 +1,32 @@
+package ru.progwards.java2.lessons.synchro.app.service;
+
+import ru.progwards.java2.lessons.synchro.app.model.Account;
+
+import java.util.Collection;
+
+public class FileStoreService implements StoreService {
+    @Override
+    public Account get(String id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Account> get() {
+        return null;
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public void insert(Account account) {
+
+    }
+
+    @Override
+    public void update(Account account) {
+
+    }
+}
